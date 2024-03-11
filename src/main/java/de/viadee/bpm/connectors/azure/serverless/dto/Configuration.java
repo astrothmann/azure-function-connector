0719befc3b48df0af252a674caf07ae576088e63
Andre Strothmann <andre.strothmann@viadee.de>
2024-03-11 18:51:37 +0100

    wip

---
+++ src/main/java/de/viadee/bpm/connectors/azure/serverless/dto/Configuration.java
@@ -0,0 +1,12 @@
+package de.viadee.bpm.connectors.azure.serverless.dto;
+
+import io.camunda.connector.generator.java.annotation.TemplateProperty;
+import jakarta.validation.constraints.NotEmpty;
+
+public record Configuration(
+    @NotEmpty
+    @TemplateProperty(group = "configuration", label = "Operation", description = "Type of operation to invoke")
+    String operation,
+    @NotEmpty
+    @TemplateProperty(group = "configuration", label = "Function Name", description = "Name of function to invoke")
+    String functioName) { }
